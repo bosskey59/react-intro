ebb4b0146d89207f3d6bad4122530d8363b588bd
Alejandro Aguilar <aaguilar2@hou-us05186-mbp13.attlocal.net>
2019-06-26 13:42:35 -0500

    added form & some lifecycle methods

---
+++ src/ExampleForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { throwStatement } from '@babel/types';
+
+export class ExampleForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name:"Pumbaa",
+            age:"16"
+        }
+    
+    //    this.formName = React.createRef();
+    //    this.formAge = React.createRef();
+    }
+    formChange = (e) =>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    
+    handleSubmit =(e)=>{
+        e.preventDefault()
+        const data = {}
+        // data.name = document.getElementById("name").value
+        // data.age = document.getElementById("age").value
+        // data.name = this.formName.current.value
+        // data.age = this.formAge.current.value
+        this.props.addPerson(this.state)
+    }
+    render() {
+        console.log(this.state)
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Name:</label>
+                <input type="text" name="name" ref={this.formName} onChange={this.formChange} value={this.state.name}/>
+                <label>Age:</label>
+                <input type="text" name="age" ref={this.formAge} onChange={this.formChange}  value={this.state.age}/>
+                <input type="submit"/>
+            </form>
+        )
+    }
+}
+
+export default ExampleForm
--- src/List.js
+++ src/List.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react'
 import Person from './person'
+import ExampleForm from './ExampleForm'
 
 
 export default class List extends Component {
     constructor(props){
         super(props)
         this.state = {
-            people: [{id:1, name:"alex", age:21},{id:2, name:"lizzie", age:23},{id:3, name:"vinh", age:20}]
+            people: [{id:1, name:"alex", age:21},{id:2, name:"lizzie", age:23},{id:3, name:"vinh", age:20}],
+            language: props.lang
         }
     }
+    // state ={
+    //     people: [{id:1, name:"alex", age:21},{id:2, name:"lizzie", age:23},{id:3, name:"vinh", age:20}]  
+    // }
+
+    // static getDerivedStateFromProps(props,state){
+    //     return {
+    //         language: props.lang,
+    //     }
+    // }
+
+    // componentDidMount(){
+    //    this.interval =  setInterval(()=>{
+    //         // going to get some 3rd party API info
+    //     },3000)
+    // }
+    // componentWillUnmount(){
+    //     clearInterval(this.interval)
+    // }
+
+    // shouldComponentUpdate(nextProps, nextState){
+    //     if (this.state.language === nextState.language){
+    //         return false
+    //     }
+    //     else {
+    //         return true
+    //     }
+    // }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+       console.log("component did update")
+    }
+
+
 
     click = (id) =>{
         console.log(id)
-
         // const filteredPpl = this.state.people.filter(person =>(
         //  person.id !== id   
         // ))
         // this.setState({
         //     people:filteredPpl
         // })
-
         this.setState((prevState) => {
             const filteredPpl = prevState.people.filter(person =>(
                 person.id !== id   
@@ -28,12 +61,24 @@ export default class List extends Component {
           })
 
     }
+    addPerson = (data) =>{
+        data.id = this.state.people.length+1
+        this.setState((prevState) => {
+            const addedPeople = [...prevState.people, data]
+            return {people:addedPeople};
+          })
+    }
+ 
     render() {
-        console.log(this.state)
+        console.log("rendering List Component")
         return (
+            <div className="hello">
             <ul>
              { this.state.people.map((person) => <Person {...person} key={person.id} click={this.click}/>)}
             </ul>
+            <ExampleForm addPerson={this.addPerson}/>
+            <h1>{this.state.language}</h1>
+            </div>
         )
     }
 }
